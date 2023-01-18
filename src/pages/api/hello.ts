da4d7f55bc93591225ddb1f3bf6e05b4f80010f3
Lemon XYZ <0xandrewblack@gmail.com>
2023-01-18 17:48:05 +0100

    Initial commit

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // prettier-ignore
+  const { method, query: { name } } = req;
+
+  switch (method) {
+    case "GET":
+      res.status(200).json({ message: `Hello ${name ? name : "World"}!` });
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).json({ message: `Method ${method} Not Allowed!` });
+  }
+}
