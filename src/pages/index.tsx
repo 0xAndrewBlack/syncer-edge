32a99d4f77fa3659b8150f0adcbde8177cc0a67a
0xAndrewBlack <andras@fekete.xyz>
2023-10-19 02:48:12 +0200

    ✨ feat(home): initial home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { FC } from "react";
+
+const Index: FC = () => {
+  return (
+    <>
+      <h1>Syncer Edge API</h1>
+
+      <p>
+        This project handles Discord/Github events/issues/thread events to sync
+        issues and tasks together.
+      </p>
+
+      <a
+        href="https://github.com/0xAndrewBlack/syncer-edge"
+        target="_blank"
+        rel="noopener noreferrer">
+        GitHub Project
+      </a>
+    </>
+  );
+};
+
+export default Index;
